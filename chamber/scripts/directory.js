2a926cf772fc8af0c9556ff95f18b47e9f975e82
anativity <foxzep@gmail.com>
2026-05-16 11:55:26 -0400

    week2 update test

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,41 @@
+const directory = document.querySelector("#directory");
+const viewGrid = document.querySelector("#viewGrid");
+const viewList = document.querySelector("#viewList");
+
+async function loadDirectory() {
+    const response = await fetch("data/chambers.json");
+    const data = await response.json();
+    displayBusinesses(data.businesses);
+}
+
+function displayBusinesses(businesses) {
+    directory.innerHTML = "";
+
+    businesses.forEach(biz => {
+        const card = document.createElement("section");
+        card.classList.add("card");
+
+        card.innerHTML = `
+      <img src="${biz.image}" alt="${biz.name} logo">
+      <h2>${biz.name}</h2>
+      <p>${biz.address}</p>
+      <p>${biz.phone}</p>
+      <a href="${biz.website}" target="_blank">Visit Website</a>
+      <p class="membership">${biz.membership} Member</p>
+    `;
+
+        directory.appendChild(card);
+    });
+}
+
+viewGrid.addEventListener("click", () => {
+    directory.classList.add("grid");
+    directory.classList.remove("list");
+});
+
+viewList.addEventListener("click", () => {
+    directory.classList.add("list");
+    directory.classList.remove("grid");
+});
+
+loadDirectory();
